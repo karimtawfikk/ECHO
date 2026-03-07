da5ab1b29900a0e657f20770696e216e3b9f08d6
Karim Tawfik <karimtawfik481@gmail.com>
2026-03-07 13:52:21 +0200

    start phase 2, edit sql query for more efficiency

--- experiments/chatbot/echo_chatbot/phase2.py
+++ experiments/chatbot/echo_chatbot/phase2.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[3]))
-import gc
 import warnings
 import os
 from dotenv import load_dotenv
@@ -12,18 +11,17 @@ from langgraph.graph.message import add_messages
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_groq import ChatGroq  # New Cloud Provider
-from sentence_transformers import SentenceTransformer
-import chromadb
+from langchain_groq import ChatGroq
+from langchain_cloudflare import CloudflareWorkersAIEmbeddings
 from pathlib import Path
 import numpy as np
 from sqlalchemy.orm import Session
 from src.db.session import engine
-from src.models import Pharaoh, PharaohText, Landmark, LandmarkText
 from sqlalchemy import text
 import yaml
 warnings.filterwarnings("ignore")
 
+load_dotenv()
 
 def load_resources():
     base_path = Path(__file__).parent / "resources"
@@ -35,18 +33,18 @@ def load_resources():
     return prompts, sql_query
 
 PROMPTS, VECTOR_SQL = load_resources()
-COLLECTION_NAME = "pharaohs"
-EMBEDDING_MODEL_NAME = "Qwen/Qwen3-Embedding-0.6B"
-EMBEDDING_MODEL_PATH = "models\Qwen3-Embedding-0.6B"
 GROQ_API_KEY1= os.getenv("GROQ_API_KEY1")
 GROQ_API_KEY2= os.getenv("GROQ_API_KEY2")
-GROQ_GENERATOR_MODEL_NAME = "llama-3.3-70b-versatile"
+
+CF_WORKERSAI_ACCOUNTID=os.getenv("R2_ACCOUNT_ID")
+CF_AI_API=os.getenv("CF_AI_API")
+
+GROQ_GENERATOR_MODEL_NAME = "openai/gpt-oss-120b"
 GROQ_QUERY_RERWRITER_MODEL_NAME = "qwen/qwen3-32b"
 TOP_K = 3
 EMBEDDING_DIM = 768
 ENTITY_NAME="Ramesses II"
 
-load_dotenv()
 
 # LangGraph state definition
 class AgentState(TypedDict):
@@ -57,18 +55,21 @@ class AgentState(TypedDict):
     response: str
 
 #Embedding model
-embedding_model = SentenceTransformer(
-    EMBEDDING_MODEL_NAME,
-    trust_remote_code=True,
-    device="cuda"
+embedding_model = CloudflareWorkersAIEmbeddings(
+    account_id=CF_WORKERSAI_ACCOUNTID,
+    api_token=CF_AI_API,
+    model_name="@cf/qwen/qwen3-embedding-0.6b"
 )
 
 def get_embedding(text: str):
-    embeddings = embedding_model.encode([text], normalize_embeddings=True)
-    embeddings = embeddings[:, :EMBEDDING_DIM]
-    query_embedding = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
-    return query_embedding[0].tolist()
+    embeddings = np.array(embedding_model.embed_query(text))
+    embeddings = embeddings / np.linalg.norm(embeddings)
+    sliced_embedding = embeddings[:EMBEDDING_DIM]
 
+    norm = np.linalg.norm(sliced_embedding)
+    final_embedding = sliced_embedding / norm if norm > 0 else sliced_embedding
+
+    return final_embedding.tolist()
 
 # Cloud LLM
 query_rewriter_llm = ChatGroq(
@@ -84,10 +85,14 @@ query_rewriter_llm = ChatGroq(
 
 generator_llm = ChatGroq(
     model_name=GROQ_GENERATOR_MODEL_NAME,
-    temperature=0.4,
+    temperature=0.8,
     max_tokens=4096,
     top_p=0.95,
     api_key=GROQ_API_KEY2,   
+    extra_body={
+        "reasoning_effort": "medium",
+        "reasoning_format": "hidden"
+    }
 )
 
 #  PROMPT & CHAIN
@@ -100,8 +105,16 @@ llm_chain = llm_prompt_template | generator_llm | StrOutputParser()
 
 #LangGraph nodes definition
 def rewrite_node(state: AgentState) -> dict:
+
+    clean_dialouge = [
+        msg for msg in state['messages'][:-1] # Exclude current user input
+        if isinstance(msg, HumanMessage) or getattr(msg, "name", None) == "search_query"
+    ]
+    
+    history_window = clean_dialouge[-10:] if clean_dialouge else []
+
     dialogue = []
-    for msg in state['messages'][:-1]:
+    for msg in history_window:
         if isinstance(msg, HumanMessage):
             role = "User "
             dialogue.append(f"{role}: {msg.content}")
@@ -111,11 +124,11 @@ def rewrite_node(state: AgentState) -> dict:
 
     history_str = "\n".join(dialogue) if dialogue else "No history yet."
 
-    print("\n\n")
+    """print("\n\n")
     print("="*50)
-    print("Rewrite History",history_str)
+    print("Rewrite History: ",history_str)
     print("="*50)
-    print("\n\n")
+    print("\n\n")"""
 
 
     search_q = rewrite_chain.invoke({"query": state['query'],
@@ -129,24 +142,28 @@ def retrieve_node(state: AgentState) -> dict:
     query_embedding = get_embedding(state['search_query'])
     
     with Session(engine) as session:
-        
-        pharaoh = session.query(Pharaoh).filter_by(name=ENTITY_NAME).first()
-        if not pharaoh: return {"context": []}
-
         raw_query = text(VECTOR_SQL)
         result = session.execute(raw_query, {
-            "p_id": pharaoh.id,
-            "embedding": str(query_embedding),
-            "limit": TOP_K
-        })
+                "pharoah_name": ENTITY_NAME,
+                "embedding": str(query_embedding),
+                "limit": TOP_K
+                })
 
         context = [row[0] for row in result]
 
     return {"context": context}
 
 def generate_node(state: AgentState) -> dict:
+    
+    clean_dialogue = [
+        msg for msg in state['messages'] 
+        if isinstance(msg, HumanMessage) or getattr(msg, "name", None) == "generator_response"
+    ] #neglect search_query messages
+
+    history_window = clean_dialogue[-11:-1] if len(clean_dialogue) > 1 else [] #last 5 turns.(10 so 5 turns)
+
     dialogue = []
-    for msg in state['messages'][:-1]: #skupping the last message which is the current user query
+    for msg in history_window: 
         if isinstance(msg, HumanMessage) :
             role = "User "
             dialogue.append(f"{role}: {msg.content}")
@@ -157,18 +174,18 @@ def generate_node(state: AgentState) -> dict:
     
     history_str = "\n".join(dialogue) if dialogue else "No previous conversation."
 
-    print("\n\n")
+    """print("\n\n")
     print("="*50)
     print("Generator History",history_str)
     print("="*50)
-    print("\n\n")
+    print("\n\n")"""
 
     print(ENTITY_NAME, ": ")
     response_text=""
     for chunk in llm_chain.stream({ #Token-level LLM stream, not LG
         "pharaoh_name": ENTITY_NAME,
         "context": "\n\n".join(state['context']),
-        "query": state['search_query'],
+        "query": state['query'],
         "chat_history": history_str,
     }):
         print(chunk, end="", flush=True)
