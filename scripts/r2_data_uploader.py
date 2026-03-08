07ece82af832026c26f6fd3c3acf1850c53db323
Karim Tawfik <karimtawfik481@gmail.com>
2026-03-08 14:59:46 +0200

    synchronize data in R2 bucket, refactor code in phase 4 chatbot

--- scripts/r2_data_uploader.py
+++ scripts/r2_data_uploader.py
@@ -21,13 +21,13 @@ client = session.client(
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY,
 )
-data_folder = Path("data/chatbot/raw/pharaohs_docs") #src
+data_folder = Path("data/video_generation/outputs/pharaohs_descriptions") #src
 
 
 files = [f for f in data_folder.rglob("*") if f.is_file()]
 
 print(f"Found {len(files)} files to upload")
-DEST_PREFIX = "data/chatbot/raw/"
+DEST_PREFIX = "data/video_generation/outputs/"
 for file_path in tqdm(files, desc="Uploading files"):
     relative = file_path.relative_to(data_folder.parent)
     key = DEST_PREFIX + str(relative).replace('\\', '/')
