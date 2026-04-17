c4675877036fdd4c4afe7770c176e0c824020f42
MalakDiab-04 <malak.amir.diab.0901@gmail.com>
2026-04-18 00:09:00 +0200

    update frontend packages and read.md file

--- FrontEnd/lib/api.ts
+++ FrontEnd/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8010";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
