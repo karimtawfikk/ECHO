1b685f101756692a859a8a3fac60b10851b46e8a
karimtawfikk <karimtawfik481@gmail.com>
2026-06-30 10:53:09 +0300

    refactor

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -100,7 +100,7 @@ export default function HomePage() {
           >
 
             <img
-              src="/images/backgrounds/xx.jpeg"
+              src="/images/backgrounds/x.png"
               alt="Background Desktop"
               className="hidden md:block w-full h-full object-cover object-top opacity-60"
               onError={(e) => { e.currentTarget.style.display = 'none'; }}
